ad3bde526d11c174c4e4222bb1fb7654560e50a5
chic-page-6502 <zacharytq@gmail.com>
2020-01-11 02:38:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
-class CreateShows < ActiveRecord::Migration
+class CreateShows < ActiveRecord::Migration[5.2]
   def change
-    create_table shows do |t|
+    create_table :shows do |t|
       t.string :name
       t.string :network
       t.string :day
